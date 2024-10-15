55813123f63c6313d0989df315370a065cc7fca5
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2024-10-15 20:36:13 +0300

    Update _app.tsx

--- my-next-app/src/pages/_app.tsx
+++ my-next-app/src/pages/_app.tsx
@@ -2,13 +2,20 @@ import { NextUIProvider } from "@nextui-org/react";
 import "../../public/styles/NextUi.css";
 import type { AppProps } from "next/app";
 import React from "react";
+import AppProvider from "@/context/AppProvider";
+import NavigationBar from "@/layouts/NavigationBar";
 
-function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
+  const AnyComponent = Component as any;
+
   return (
     <NextUIProvider>
-      <Component {...pageProps} />
+      <AppProvider>
+        <NavigationBar />
+        <main className="bg-orange-400 text-gray-800 flex h-screen w-full">
+          <AnyComponent {...pageProps} />
+        </main>
+      </AppProvider>
     </NextUIProvider>
   );
 }
-
-export default App;
