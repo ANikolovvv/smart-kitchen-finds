10c34a01a88735348cf032ea88384c94ad6882f9
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2024-10-15 20:36:08 +0300

    Create pageNames.ts

---
+++ my-next-app/src/layouts/NavigationBar/pageNames.ts
@@ -0,0 +1,7 @@
+export const categories = [
+    { name: 'Smart Speakers', href: '/smart-speakers' },
+    { name: 'Smart Lighting', href: '/smart-lighting' },
+    { name: 'Smart Thermostats', href: '/smart-thermostats' },
+    { name: 'Smart Cameras', href: '/smart-cameras' },
+    { name: 'Smart Locks & Security', href: '/smart-locks-security' },
+];
