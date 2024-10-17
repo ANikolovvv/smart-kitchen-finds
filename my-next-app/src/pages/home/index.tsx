30c31fd7da47b4227f66cba9e23c6282ea070640
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2024-10-17 21:01:42 +0300

    Update index.tsx

--- my-next-app/src/pages/home/index.tsx
+++ my-next-app/src/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import Box from "@/components/atoms/Box";
-import React, { FC, memo } from "react";
-
+import React, { FC } from "react";
 
 const Home: FC = () => {
-  return <Box className="h-full w-full max-h-screen "><h1>Gollllllllll</h1></Box>;
+  return (
+    <Box className="h-full w-full max-h-screen ">
+      <h1>Gollllllllll</h1>
+    </Box>
+  );
 };
 
-export default Home
-
+export default Home;
