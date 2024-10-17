7bbf1ffbbcadd93582c2b5d6b614570e3f42536b
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2024-10-17 21:01:50 +0300

    Create index.tsx

---
+++ my-next-app/src/pages/speakers/index.tsx
@@ -0,0 +1,9 @@
+import Box from "@/components/atoms/Box";
+import React, { FC } from "react";
+
+
+const Speakers: FC = () => {
+  return <Box className="h-full w-full max-h-screen "><h1>Speakerssss</h1></Box>;
+};
+
+export default Speakers
