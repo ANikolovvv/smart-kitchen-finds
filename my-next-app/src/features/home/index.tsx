02c211a5e0cc298b31183dba85ac6ff7c7ce7d1b
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2024-11-10 13:50:59 +0200

    Update index.tsx

--- my-next-app/src/features/home/index.tsx
+++ my-next-app/src/features/home/index.tsx
@@ -5,18 +5,31 @@ import { featuredProducts } from "./featuredProducts";
 
 const HomePage: FC = () => {
   return (
-    <Box className="flex-grow w-full p-4">
-      <h1 className="text-2xl md:text-3xl font-bold text-center mb-6">
-        Discover Smart Home Devices
-      </h1>
-      <p className="text-center mb-10 text-gray-600">
-        Explore top-rated smart devices to elevate your home’s connectivity and
-        convenience.
-      </p>
-      <Box className="flex flex-wrap justify-center gap-4">
-        {featuredProducts.map((product) => (
-          <ProductCard key={product.title} {...product} />
-        ))}
+    <Box
+      className="flex-grow w-full p-4 rounded-lg shadow-lg"
+      style={{
+        backgroundImage:
+          "url('https://alnasser.solutions/wp-content/uploads/2024/08/Smart-lighting.jpeg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        borderRadius: "16px", // заоблени ъгли
+        boxShadow: "0 4px 20px rgba(255, 165, 0, 0.5)",
+      }}
+    >
+      <Box className="max-w-4xl mx-auto text-center p-6">
+        <h1 className="text-2xl md:text-3xl font-bold mb-4">
+          Discover Smart Home Devices
+        </h1>
+        <p className="mb-6 text-gray-700">
+          Explore top-rated smart devices to elevate your home’s connectivity
+          and convenience.
+        </p>
+        <Box className="flex flex-wrap justify-center gap-2">
+          {featuredProducts.map((product) => (
+            <ProductCard key={product.title} {...product} />
+          ))}
+        </Box>
       </Box>
     </Box>
   );
