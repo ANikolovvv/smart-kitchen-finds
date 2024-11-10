a47c741e20db8fa7a596912dce00c68538c54a5d
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2024-11-10 13:51:06 +0200

    Update index.tsx

--- my-next-app/src/features/home/ProductCard/index.tsx
+++ my-next-app/src/features/home/ProductCard/index.tsx
@@ -5,24 +5,13 @@ import Link from "next/link";
 type ProductCardProps = {
   title: string;
   description: string;
-  imageUrl: string;
   link: string;
 };
 
-const ProductCard: FC<ProductCardProps> = ({
-  title,
-  description,
-  imageUrl,
-  link,
-}) => (
-  <Card className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2 m-2 shadow-md transition-transform transform hover:scale-105">
+const ProductCard: FC<ProductCardProps> = ({ title, description, link }) => (
+  <Card className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 p-2 m-1 shadow-lg transition-transform transform hover:scale-105">
     <CardHeader className="font-bold text-lg">{title}</CardHeader>
     <CardBody>
-      <img
-        src={imageUrl}
-        alt={`${title} image`}
-        className="rounded-md w-full h-40 object-cover mb-2"
-      />
       <p className="text-sm mb-2">{description}</p>
       <Link
         href={link}
