19d4130efe00df679030dd2b6e1f31a5c3b238b8
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2024-11-16 19:17:29 +0200

    Update index.tsx

--- my-next-app/src/components/atoms/PageWrapper/index.tsx
+++ my-next-app/src/components/atoms/PageWrapper/index.tsx
@@ -1,30 +1,34 @@
 import { FC, memo, ReactNode } from "react";
 import Box from "../Box";
-import Title from "../Title";
 
 type PageWrapperProps = {
   children: ReactNode;
+  backgroundImage?: string; 
 };
 
-const PageWrapper: FC<PageWrapperProps> = ({ children }) => {
+const PageWrapper: FC<PageWrapperProps> = ({ children, backgroundImage }) => {
   return (
     <Box
-      role="region"
-      aria-labelledby="page-title"
       className="flex w-full font-serif p-4 rounded-lg shadow-lg"
       style={{
-        backgroundImage: "url('/images/home-image.jpg')",
+        backgroundImage: backgroundImage ? `url('${backgroundImage}')` : undefined,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
-        borderRadius: "12px", // Updated border radius for consistent styling
+        borderRadius: "12px",
         boxShadow: "0 2px 15px rgba(255, 165, 0, 0.5)",
       }}
     >
-      <Box className="max-w-4xl mx-auto font-serif text-center pt-1 p-4 sm:p-6 md:p-8 md:pt-2">
+      <Box
+        role="region"
+        aria-labelledby="page-title"
+        className="max-w-4xl mx-auto font-serif text-center pt-1 p-4 sm:p-6 md:p-8 md:pt-2"
+      >
         {children}
       </Box>
     </Box>
   );
 };
+
 export default memo(PageWrapper);
+
