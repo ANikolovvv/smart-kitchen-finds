be6bf49db93f0cdcd398c8273e828ae5f2a73220
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2024-11-16 19:18:18 +0200

    Update index.tsx

--- my-next-app/src/features/home/index.tsx
+++ my-next-app/src/features/home/index.tsx
@@ -4,14 +4,15 @@ import ProductCard from "./CategoryCard";
 import { featuredProducts } from "./featuredProducts";
 import Title from "@/components/atoms/Title";
 import PageWrapper from "@/components/atoms/PageWrapper";
+import CategoryCard from "./CategoryCard";
 
 const HomePage: FC = () => {
   return (
-    <PageWrapper>
+    <PageWrapper backgroundImage="/images/home-image.jpg">
       <Title>Welcome to the Future: Your Complete Smart Home Hub</Title>
-      <Box className="flex flex-col items-center justify-center gap-4 md:gap-6">
+      <Box className="flex flex-wrap justify-center gap-4 md:gap-6">
         {featuredProducts.map((product) => (
-          <ProductCard key={product.title} {...product} />
+          <CategoryCard key={product.title} {...product} />
         ))}
       </Box>
     </PageWrapper>
