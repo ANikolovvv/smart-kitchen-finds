ba5ffc0392fee941076e01c2114e81295ce8ae3a
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2024-11-18 20:45:00 +0200

    Update index.tsx

--- my-next-app/src/pages/blog/index.tsx
+++ my-next-app/src/pages/blog/index.tsx
@@ -1,4 +1,5 @@
 import Box from "@/components/atoms/Box";
+import CategoryArticles from "@/components/atoms/CategoryArticles";
 import PageWrapper from "@/components/atoms/PageWrapper";
 import Title from "@/components/atoms/Title";
 import React, { FC } from "react";
@@ -7,6 +8,7 @@ const Blog: FC = () => {
   return (
     <PageWrapper>
       <Title>The Smart Life Blog: Expert Advice and Smart Home Hacks</Title>
+      <CategoryArticles />
     </PageWrapper>
   );
 };
