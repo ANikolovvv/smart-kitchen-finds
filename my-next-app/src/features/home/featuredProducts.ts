14a0a43899e0321112d9e636ff5090565683acfb
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2025-03-03 15:51:39 +0200

    Update featuredProducts.ts

--- my-next-app/src/features/home/featuredProducts.ts
+++ my-next-app/src/features/home/featuredProducts.ts
@@ -2,37 +2,37 @@ export const featuredProducts = [
   {
     title: "Smart Speaker",
     description: "High-quality smart speaker with voice assistant.",
-    imageUrl: "/images/home-image.jpg",
+    imageUrl: "/images/speaker_image.jpg",
     link: "/speakers",
   },
   {
     title: "Smart Thermostat",
     description: "Control your home's temperature from anywhere.",
-    imageUrl: "/images/home-image.jpg",
+    imageUrl: "/images/thermostat.jpg",
     link: "/thermostats",
   },
   {
     title: "Smart Lighting",
     description: "Adjust lighting and ambiance with voice control.",
-    imageUrl: "/images/home-image.jpg",
+    imageUrl: "/images/lighting.jpg",
     link: "/lighting",
   },
   {
     title: "Smart Lock",
     description: "Secure your home with a remote-controlled smart lock.",
-    imageUrl:"/images/home-image.jpg",
+    imageUrl:"/images/locks.jpg",
     link: "/locks-security",
   },
   {
     title: "Smart Camera",
     description: "Monitor your home remotely with real-time video.",
-    imageUrl: "",
+    imageUrl: "/images/camera.jpg",
     link: "/cameras",
   },
   {
     title: "Blog",
     description: "Explore the latest innovations shaping the future of technology.",
-    imageUrl: "/images/home-image.jpg",
+    imageUrl: "/images/blog.jpg",
     link: "/blog",
   },
 
